14de6c9f59de6de7b6dfa5cf323e62fc5a567aa2
TheSirTaco7 <114195041+TheSirTaco7@users.noreply.github.com>
2024-01-31 06:56:12 -0500

    updated character

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
 //Hi Mr. Collins, this is Silas Moore. I would like to inform you that Dean Owen Dorner still smells and is a dork. Have a good day.
 
-//xPos, yPos, xMomentum, yMomentum, scale, name
-let player = [100, 100, 0, 0, 20, "Player"]
+//           xPos, yPos, xMomentum, yMomentum, rotation, scale, name
+let player = [100, 100,  0,          0,        0,         60,    "Player"]
+let bots =   [
+//xPos, yPos, xMomentum, yMomentum, rotation, scale, color,        name
+  [10,  10,   0,          0,        0,         20,   (30, 50, 90), "Bot0"],
+
+]
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(0, 40, 60);
-  circle(player[0], player[1], player[4]);
+  placePlayer(player);
 }
 
-function draw() {
+function draw() { 
 
+  movePlayer();
+
+  clear()
+  background(0, 40, 60);
+  placePlayer(player);
+
+}
+
+function movePlayer(){
   if(keyIsDown(LEFT_ARROW)){
     if(player[2] > -8){player[2]-=0.5}
   }else if(keyIsDown(RIGHT_ARROW)){
@@ -26,12 +40,15 @@ function draw() {
   }else{
     if (player[3] > 0){player[3]-=0.25}else if(player[3] < 0){player[3]+=0.25}
   }
-  
+}
 
-  clear()
-  background(0, 40, 60);
+function placePlayer(player) {
   player[0]+=player[2]
   player[1]+=player[3]
-  circle(player[0], player[1], player[4]);
-
+  fill(20, 255, 0);
+  circle(player[0], player[1], player[5]);
+  fill(100, 255, 200);
+  circle(player[0], player[1], player[5] * 0.7);
+  fill(200, 50, 50);
+  rect(player[0], player[1], player[5], player[5])
 }
